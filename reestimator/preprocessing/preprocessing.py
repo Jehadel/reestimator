ad321be120b7b53847dce10f305b912333b24a4d
Krystelle Gilard <k.gilard@yahoo.fr>
2021-08-25 20:03:02 +0200

    essai commit

--- reestimator/preprocessing/preprocessing.py
+++ reestimator/preprocessing/preprocessing.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 
 class Preprocessing_data:
-    def __init__(self) -> None:
-        pass
+    def __init__(self,df):
+        self.df=df
 
     def conv_int(col):
         """
@@ -14,16 +14,16 @@ class Preprocessing_data:
         # to do : check if column mixed str (ex. '2A') and int. col.str.isnumeric()==False
         return pd.to_numeric(col, downcast='integer')
 
-    def conv_downcast(df):
+    def conv_downcast(self):
         """
         Downcast numeric dtypes in dataframe df to save memory
         """
-        float_cols = df.select_dtypes('float').columns
-        int_cols = df.select_dtypes('integer').columns
+        float_cols = self.df.select_dtypes('float').columns
+        int_cols = self.df.select_dtypes('integer').columns
 
-        df[float_cols] = df[float_cols].apply(pd.to_numeric, downcast='float')
-        df[int_cols] = df[int_cols].apply(pd.to_numeric, downcast='integer')
-        return df
+        self.df[float_cols] = self.df[float_cols].apply(pd.to_numeric, downcast='float')
+        self.df[int_cols] = df[int_cols].apply(pd.to_numeric, downcast='integer')
+        return self.df
 
     def conv_date(col):
         """
@@ -31,15 +31,15 @@ class Preprocessing_data:
         """
         return pd.to_datetime(col, format='%Y-%m-%d')
 
-    def drop_rows_of_specific_column(df, col_name):  #df dataframe
+    def drop_rows_of_specific_column(self, col_name):  #df dataframe
         """
         Drop rows of specific columns with Nan
         """
-        mod_df = df.dropna(how='any', subset=[col_name])
+        mod_df = self.df.dropna(how='any', subset=[col_name])
         return mod_df
 
     # A REMPLACER PAR REQUETE SQL ???
-    def remplacement_mutation(df):
+    def remplacement_mutation(self):
         """
         Remplace Sale by 1 and Others type of mutation data by 0 in nature_mutation column of df
         """
@@ -52,15 +52,15 @@ class Preprocessing_data:
             'Adjudication': "0",
             'Expropriation': "0"
         }
-        df['nature_mutation'] = df['nature_mutation'].replace(
+        self.df['nature_mutation'] = df['nature_mutation'].replace(
             replacement_mutation_dict)
 
-        return df
+        return self.df
 
-    def cadastral_sector(df):
+    def cadastral_sector(self):
         """
         Get secteur_cadastral from id_parcelle and add a column to df
         """
-        df["secteur_cadastral"] = df["id_parcelle"].str.slice(5, 10)
+        self.df["secteur_cadastral"] = self.df["id_parcelle"].str.slice(5, 10)
 
-        return df
+        return self.df
