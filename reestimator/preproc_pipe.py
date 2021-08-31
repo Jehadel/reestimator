082dbc281bed70b14264bb9e4ad1e803c64bcd27
Anna-PL <annaplokhikh@yahoo.fr>
2021-08-31 11:26:20 +0200

    file ununsed, worked with Krystelle on other branch

---
+++ reestimator/preproc_pipe.py
@@ -0,0 +1,44 @@
+class Preproc:
+    def __init__(self, df):
+        self.df = df
+    def temp_test():
+        return "OK structures on going"
+    def remove_dom_tom(self, df9):
+        """
+        Function used to choose the meothod to limit the min and max outliers
+        parameters
+        ----------
+        df : pandas dataframe
+            raw data ()
+        returns:
+        --------
+        df : pandas dataframe
+            filtered data set
+        """
+        return
+    def get_outliers_limits():
+        """
+        Function used to choose the meothod to limit the min and max outliers
+        """
+        return
+    def scaled_data(self):
+        """
+        Function used to choose the method to scale the data
+        """
+        pass
+    def encoding_data(df):
+        """
+        Function that execute one hot encoding or another methode to encode the
+        """
+        pass
+    def train_model(X_train, y_train):
+        """
+        Function that trains the model
+        """
+        pass
+    def save_model(reg):
+        """
+        Method that saves the model into a .joblib file
+        and uploads it on Google Storage /models folder
+        """
+        pass
