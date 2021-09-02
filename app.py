16cf70764669a2c23d75810cb9143a73429a28a6
Krystelle Gilard <k.gilard@yahoo.fr>
2021-09-03 01:02:35 +0200

    last test

--- app.py
+++ app.py
@@ -1,32 +1,30 @@
 import streamlit as st
-import sqlalchemy
-import pymysql
 import requests
 import folium
 #Importing our Packages: Analytics
 import pandas as pd
 import numpy as np
-​
+# ​.
 #Viz libaries
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd # library for data analysis
 from geopy.geocoders import Nominatim
-​
+#
 # convert an address into latitude and longitude values
 import requests # library to handle requests
 import folium # map rendering library
 import streamlit as st #creating an app
 #from streamlit_folium import folium_static
-​
+# ​
 #Web App Libraries
 from streamlit_folium import folium_static
-​
+# ​
 #Group Paris, Marseille, Lyon by arrondissement
 def transform_string(string, separator):
     L = string.split(separator)
     return L[0] if L[0] in ['Paris', 'Marseille', 'Lyon'] else L[0]
-​
+# ​
 #Get data from Mysql on Gcloud
 df2 = pd.read_csv('final_data.csv', dtype={'code_dep':'object'})
 url = 'https://raw.githubusercontent.com/gmanchon/streamlit/master/data/departements.json'
@@ -34,7 +32,7 @@ response = requests.get(url).json()
 df2= df2[~df2['code_dep'].isin(['974','973','972', '971'])]
 df_gv = df2[df2['code_dep'].isin(['13','75','69'])]
 df2.commune = df2.commune.apply(lambda x: transform_string(x, ' '))
-​
+# ​
 #Progress Bar
 latest_iteration = st.empty()
 bar = st.progress(0)
@@ -44,46 +42,46 @@ for i in range(100):
     latest_iteration.text(f'Itération {i+1}')
     bar.progress(i + 1)
     time.sleep(0.01)
-​
+# ​
 #Filters
 type_bati = st.sidebar.selectbox(
     'Type de batiment',
     ('Maison', 'Appartement')
 )
-​
+# ​
 #Slider
 df2.Year = df2.Year.astype('int')
 values = st.slider(
     'Selectionner les annees desirees',
      2016, 2020, (2016, 2020))
 st.write('Valeurs:', values)
-​
+# ​
 param = st.sidebar.selectbox(
     'Metrics',
     ('Prixm2', 'transactions', 'Avg_sqm', 'No_rooms', 'price', 'Surface'))
-​
+# ​
 # Add a selectbox to the sidebar:
 selectbox_city = st.sidebar.selectbox(
     'Ville',
     ('Marseille', 'Paris', 'Lyon'))
-​
+# ​
 if st.checkbox('Sans Métropoles Majeures ?'):
     df2= df2[~df2['commune'].isin(['Paris','Marseille','Lyon'])]
-​
+# ​
 #Filter Final Dataframe
 df2= df2[df2['type']== type_bati]
 df2= df2[df2['Year'].between(values[0],values[1])]
-​
+# ​
 # Create Final Sum Checkbox
 if st.checkbox('Somme?'):
     df2 = df2.groupby(['code_dep','type'])[f"{param}"].sum().reset_index(drop=False)
-​
+# ​
 df2 = df2.groupby(['code_dep','type'])[f"{param}"].mean().reset_index(drop=False)
-​
+# ​
 #Filter Departements in the Final DF
 df2.code_dep = df2.code_dep.astype('str')
 geodf = df2[['code_dep',f'{param}']]
-​
+# ​
 #Center the Map around the Point of Interest
 def center(ville):
    address = f'{ville}, FR'
@@ -92,11 +90,11 @@ def center(ville):
    latitude = location.latitude
    longitude = location.longitude
    return latitude, longitude
-​
+# ​
 # initialize the first map and store it in a m object
 m = folium.Map(location=center('Paris'), zoom_start=5)
-​
-​
+# ​
+# ​
 def threshold(data, param):
         threshold_scale = np.linspace(geodf[param].min(),
                                       geodf[param].max(),
@@ -105,8 +103,8 @@ def threshold(data, param):
         threshold_scale = threshold_scale.tolist()
         threshold_scale[-1] = threshold_scale[-1]
         return threshold_scale
-​
-​
+# ​
+# ​
 def show_maps(data, threshold_scale):
             folium.Choropleth(geo_data=response,
                   name="choropleth",
@@ -118,30 +116,30 @@ def show_maps(data, threshold_scale):
                   line_opacity=.2,
                   legend_name=f"{param} par commune par departement ",
                   nan_fill_color='white').add_to(m)
-​
+# ​
 
             folium_static(m)
-​
-​
+# ​
+# ​
 show_maps(geodf, threshold(geodf, param))
-​
+# ​
 folium.LayerControl().add_to(m)
 '''
 # Distribution des paramètres clefs sur les villes principales de France
 '''
-​
+# ​
 # Format second map dataframe
 df_gv= df_gv[df_gv['type']== type_bati]
 df_gv= df_gv[df_gv['Year'].between(values[0],values[1])]
 df_gv = df_gv.groupby(['code','type'])[f"{param}"].mean().reset_index(drop=False)
 df_gv.code = df_gv.code.astype('str')
 geodf2 = df_gv[['code',f'{param}']]
-​
+# ​
 import requests
 url2 = 'https://raw.githubusercontent.com/Jehadel/reestimator/JulienVis/notebooks/Julien/arrondissements-millesimes0.geojson'
 response2 =  requests.get(url2).json()
-​
-​
+# ​
+# ​
 #Display the second map
 m2 = folium.Map(location=center(f'{selectbox_city}'), zoom_start=11)
 def show_maps2(data, threshold_scale):
@@ -157,19 +155,19 @@ def show_maps2(data, threshold_scale):
                   nan_fill_color='white').add_to(m2)
 
             folium_static(m2)
-​
+# ​
 #                threshold_scale=[-10, 60, 70, 80, 90, 100]
 show_maps2(geodf2, threshold(geodf2, param))
 folium.LayerControl().add_to(m2)
-​
-​
+# ​
+# ​
 st.markdown('''# Distribution des biens Fonciers à Marseille''')
-​
+# ​
 typelocal = st.radio(
      "",
      ('Maisons', 'Appartements'))
-​
-​
+# ​
+# ​
 # Show Marseille Images
 from PIL import Image
 if typelocal == 'Appartements':
@@ -188,19 +186,19 @@ st.markdown('''## Prix/m² en fonction du type de logement
 type_local = st.radio(
      "",
      ('Maison', 'Appartement'))
-​
+# ​
 '''
 ### Surface en m² du bien à estimer?
 '''
 surface = st.slider('', 0, 500, 25)
-​
+# ​
 """
 ### Nombre de pièces principales
 """
 pieces = st.slider('', 0, 1, 8)
 value = list(range(1, 17))
 
-​
+# ​
 """
 ### Arrondissement
 """
