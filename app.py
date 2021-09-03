044fd986f4f75a728997caa291f72fd4000f3044
Krystelle Gilard <k.gilard@yahoo.fr>
2021-09-03 10:57:14 +0200

    debug predict api

--- app.py
+++ app.py
@@ -269,7 +269,7 @@ param_dict = {
     "Appartement": [int(is_house)],
     "Maison": [int(is_appart)]
     }
-url = "https://reestimatordocker-jw6jz6q2fq-ew.a.run.app"
+url = "https://reestimatordocker-jw6jz6q2fq-ew.a.run.app/predict"
 if st.button('Prix estimé'):
     response = requests.get(url, params=param_dict).json()
     col1, col2 = st.columns(2)
